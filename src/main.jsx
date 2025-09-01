4a21547cd79d87f1e0921f235630f0a64e27aeb3
jonas_nm <72455215+Nosperadu@users.noreply.github.com>
2025-09-01 11:43:08 +0200

    setup pages deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
+// src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter, Routes, Route } from "react-router-dom";
-import App from "./App";
+import Home from "./pages/Home.jsx";
 import SeriesPage from "./pages/SeriesPage.jsx";
 import "./styles.css";
 
 createRoot(document.getElementById("root")).render(
   <HashRouter>
     <Routes>
-      <Route path="/" element={<App />} />
+      <Route path="/" element={<Home />} />
       <Route path="/series/:id" element={<SeriesPage />} />
     </Routes>
   </HashRouter>
